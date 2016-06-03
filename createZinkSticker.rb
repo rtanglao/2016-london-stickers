ebdfea9a6cae3d50ebec6095168a294075cf65bd
Roland Tanglao <roland@rolandtanglao.com>
2016-06-03 16:24:55 -0700

    first working version

--- createZinkSticker.rb
+++ createZinkSticker.rb
@@ -4,6 +4,7 @@ require 'pp'
 require 'hpricot_scrub'
 require 'open-uri'
 require 'uri'
+require 'oily_png'
 
 if ARGV.length < 1
   puts "usage: #{$0} uri"
@@ -17,10 +18,30 @@ uri = ARGV[0]
 puts uri
 doc = Hpricot(open(uri).read)
 str = doc.scrub.to_s.strip.gsub(/\s+/, " ").gsub(/\s+/, "")
-$stderr.puts str
+$stderr.puts str.length
+column = 0
+row = 0
+png = ChunkyPNG::Image.new(1120, 620, ChunkyPNG::Color::TRANSPARENT)
+output_jpg = false
 str.each_byte do |byte|
+  $stderr.printf("column:%d, row:%d\n", column, row)
   r,g,b = green_array[byte % 5][0],green_array[byte % 5][1],
           green_array[byte % 5][2]
   $stderr.printf("r:%d, g:%d, b:%d\n",r,g, b)
+  175.times do
+    png[column,row] = ChunkyPNG::Color.rgb(r, g, b)  
+    column += 1
+    if column == 1120
+      row += 1
+      column = 1
+    end
+    if row == 620
+      output_jpg = true
+      break
+    end
+  end
+  break if output_jpg
 end
+png.save('p.png')
+
 
