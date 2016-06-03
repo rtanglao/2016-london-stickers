9a6c2df568c83630a45e89677e61d627041bcbde
Roland Tanglao <roland@rolandtanglao.com>
2016-06-03 15:59:28 -0700

    got r g b working

--- createZinkSticker.rb
+++ createZinkSticker.rb
@@ -10,13 +10,17 @@ if ARGV.length < 1
   exit
 end
 
-green_array = [ 0xC3DBB4, 0xD3E2B6, 0xAACCB1, 0x87BDB1, 0x68B3AF]
+green_array = [ [0xC3, 0xDB, 0xB4], [0xD3, 0xE2, 0xB6],
+                [0xAA, 0xCC, 0xB1],  [0x87, 0xBD, 0xB1],
+                 [0x68, 0xB3, 0xAF]]
 uri = ARGV[0]
 puts uri
 doc = Hpricot(open(uri).read)
 str = doc.scrub.to_s.strip.gsub(/\s+/, " ").gsub(/\s+/, "")
 $stderr.puts str
 str.each_byte do |byte|
-  puts green_array[byte % 5]
+  r,g,b = green_array[byte % 5][0],green_array[byte % 5][1],
+          green_array[byte % 5][2]
+  $stderr.printf("r:%d, g:%d, b:%d\n",r,g, b)
 end
 
